0ccf16dc698a3402aecf1c58535227b7c9cd98b1
ADuan2003 <123067489+ADuan2003@users.noreply.github.com>
2023-08-27 22:31:35 -0400

    Create main.cpp
    Tester file

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include PR.h
+int main () {
+    vector<string> p = {"Nationalist", "SDP", "Christian Social", "Green", "Communist", "Liberal", "Cascadian"};
+    vector<int> v = {56789, 293344, 29393, 65745, 10035, 949424, 8123};
+    PR dh0 (p, v, 'd', 0, 69);
+    PR hare8 (p, v, 'h', 8, 69);
+}
