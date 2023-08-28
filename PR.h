2d65b97ec4530c0c3f32302ce477d65d8bea9bd1
ADuan2003 <123067489+ADuan2003@users.noreply.github.com>
2023-08-27 23:19:49 -0400

    Update PR.h
    Post-testing :D

--- PR.h
+++ PR.h
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <queue>
+#include <cmath>
 using namespace std;
 class PR {
     public:
@@ -13,106 +15,119 @@ class PR {
     priority_queue<pair<int, pair<string, int>>> pq;
     PR (vector<string>& p, vector<int>& v, char type, double threshold, int numSeats) {
         total = 0;
-        pq.clear();
+        while (!pq.empty()) {
+            pq.pop();
+        }
         sets.clear();
-        for (int count = 0; count < min(p.size(), v.size()), count++) {
+        for (int count = 0; count < min(p.size(), v.size()); count++) {
             total+=v[count];
         }
-        for (int count = 0; count < min(p.size(), v.size()), count++) {
+        int temp = 0;
+        for (int count = 0; count < min(p.size(), v.size()); count++) {
+            sets.push_back(0);
             if (v[count] >= total*threshold/100) {
-                sets.push_back(0);
+                temp+=v[count];
                 pq.push({v[count], {p[count], count}}); //votes, party name, and position in array
             }
         }
+        total = temp;
         //lists by which has the most votes
         switch (type) {
             case 'd':
+            cout << "REPUBLICAN" << endl;
                 DHondt(numSeats);
+                break;
             case 's':
                 SaintLague(numSeats);
+                break;
             case 'h':
+            cout << "DEMOCRATIC" << endl;
                 Hare(numSeats);
+                break;
             case 'o':
                 Droop(numSeats);
+                break;
             case 'i':
                 Imperiali(numSeats);
+                break;
             case 'a':
                 Apportion(numSeats);
+                break;
         }
-        for (int count = 0; count < sets.size(); count++) {
+        for (int count = 0; count < v.size(); count++) {
             cout << p[count] << " " << v[count] << " " << sets[count] << endl;
         }
-    }
-    //highest averages
-    DHondt(int seats) {
+    };
+    void DHondt(int);
+    void SaintLague(int);
+    void Apportion(int);
+    void Hare(int);
+    void Droop(int);
+    void Imperiali(int);
+};
+//highest averages
+    void PR::DHondt(int seats) {
         for (int count = 1; count <= seats; count++) {
             int vs = pq.top().first;
-            int py = pq.top().second.first;
+            string py = pq.top().second.first;
             int index = pq.top().second.second;
             pq.pop();
-            cout << count << " seat awarded to " <<< py << endl;
+            cout << count << " seat awarded to " << py << endl;
             sets[index]++;
             int numSeats = sets[index];
-            vs = (numSeats/(numSeats+1));
+            vs = (vs*numSeats)/(numSeats+1);
             pq.push({vs, {py, index}});
         }
+        return;
     }
-    SaintLague(int seats) {
+    void PR::SaintLague(int seats) {
         for (int count = 1; count <= seats; count++) {
             int vs = pq.top().first;
-            int py = pq.top().second.first;
+            string py = pq.top().second.first;
             int index = pq.top().second.second;
             pq.pop();
-            cout << count << " seat awarded to " <<< py << endl;
+            cout << count << " seat awarded to " << py << endl;
             sets[index]++;
             int numSeats = sets[index];
-            vs = ((2*numSeats-1)/(2*numSeats+1));
+            vs = (vs*(2*numSeats-1))/(2*numSeats+1);
             pq.push({vs, {py, index}});
         }
+        return;
     }
-    Apportion(int seats) {
+    void PR::Apportion(int seats) {
         if (pq.size() > seats) {
             cout << "too many states and too few seats" << endl;
+            return;
         }
         priority_queue<pair<int, pair<string, int>>> pq2;
         int alloc = 0;
         while (!pq.empty()) {
             int vs = pq.top().first;
-            int py = pq.top().second.first;
+            string py = pq.top().second.first;
             int index = pq.top().second.second;
             pq.pop();
             sets[index]++;
             alloc++;
-            cout << alloc << " seat awarded to " <<< py << endl;
+            cout << alloc << " seat awarded to " << py << endl;
             vs = vs/sqrt(2);
             pq2.push({vs, {py, index}});
         }
         while (alloc < seats) {
             int vs = pq2.top().first;
-            int py = pq2.top().second.first;
+            string py = pq2.top().second.first;
             int index = pq2.top().second.second;
             pq2.pop();
             sets[index]++;
             alloc++;
-            cout << alloc << " seat awarded to " <<< py << endl;
+            cout << alloc << " seat awarded to " << py << endl;
             int numSeats = sets[index];
-            vs = vs*sqrt((numSeats-1)/(numSeats+1));
+            vs = (vs*sqrt(numSeats-1))/sqrt(numSeats+1);
             pq2.push({vs, {py, index}});
         }
-        for (int count = 1; count <= seats; count++) {
-            int vs = pq.top().first;
-            int py = pq.top().second.first;
-            int index = pq.top().second.second;
-            pq.pop();
-            cout << count << " seat awarded to " <<< py << endl;
-            sets[index]++;
-            int numSeats = sets[index];
-            vs = (numSeats/(numSeats+1));
-            pq.push({vs, {py, index}});
-        }
+        return;
     }
     //largest remainder
-    Hare(int seats) {
+    void PR::Hare(int seats) {
         int quota = total/seats;
         int allocated = 0;
         while (allocated < seats) {
@@ -125,18 +140,19 @@ class PR {
                     vs-=quota;
                     sets[index]++;
                     allocated++;
-                    cout << allocated << " seat awarded to " <<< py << endl;
+                    cout << allocated << " seat awarded to " << py << endl;
                 }
                 pq.push({vs, {py, index}});
             }
             else {
                 allocated++;
                 sets[index]++;
-                cout << allocated << " seat awarded to " <<< py << endl;
+                cout << allocated << " seat awarded to " << py << endl;
             }
         }
+        return;
     }
-    Droop(int seats) {
+    void PR::Droop(int seats) {
         int quota = total/(seats+1);
         quota++;
         int allocated = 0;
@@ -150,18 +166,19 @@ class PR {
                     vs-=quota;
                     sets[index]++;
                     allocated++;
-                    cout << allocated << " seat awarded to " <<< py << endl;
+                    cout << allocated << " seat awarded to " << py << endl;
                 }
                 pq.push({vs, {py, index}});
             }
             else{
                 sets[index]++;
                 allocated++;
-                cout << allocated << " seat awarded to " <<< py << endl;
+                cout << allocated << " seat awarded to " << py << endl;
             }
         }
+        return;
     }
-    Imperiali(int seats) {
+    void PR::Imperiali(int seats) {
         int quota = total/(seats+2);
         int allocated = 0;
         while (allocated < seats && !pq.empty()) {
@@ -174,16 +191,16 @@ class PR {
                     vs-=quota;
                     sets[index]++;
                     allocated++;
-                    cout << allocated << " seat awarded to " <<< py << endl;
+                    cout << allocated << " seat awarded to " << py << endl;
                 }
                 pq.push({vs, {py, index}});
             }
             else {
                 sets[index]++;
                 allocated++;
-                cout << allocated << " seat awarded to " <<< py << endl;
+                cout << allocated << " seat awarded to " << py << endl;
             }
         }
+        return;
     }
-}
 #endif 
